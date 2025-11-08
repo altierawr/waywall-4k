e1071902f80527fa80faa51cbea60b38ce3d459c
altierawr <104657457+altierawr@users.noreply.github.com>
2025-11-08 23:15:10 +0200

    reorganize config, add remap fixes & pie directory mirror

--- init.lua
+++ init.lua
@@ -1,29 +1,70 @@
 local waywall = require("waywall")
 local helpers = require("waywall.helpers")
 
+-- CONFIGS - Edit these to customize
+local sens = 1.0
+
+-- Layout for ingame (search crafting) + chatting mode. Make sure these are installed system-wide
+local ingame_layout = "norge2"
+local chatting_layout = "us"
+
+-- Colors for pie and some texts
+local primary_col = "#d08e2b"
+local secondary_col = "#222222"
+local ecount_text_color = "#ffffff"
+
+-- Chat mode text
+local chat_mode_text_x = 60
+local chat_mode_text_y = 1380
+local chat_mode_text_color = "#ffffff"
+local chat_mode_text_size = 6
+local chat_mode_text = "CHAT MODE ENABLED"
+
+-- F3 pie mirror
+local pie_output_size = 500
+local pie_x = 2500
+local pie_y = 1100
+
+-- Eye measuring overlay
+local overlay_x = 0
+local overlay_y = 400
+local overlay_width = 1600
+local overlay_height = 900
+
+-- F3 entity count mirror
+local ecount_mirror_size_mult = 6
+local ecount_mirror_x = 1000
+local ecount_mirror_y = 1000
+
+-- Pie directory mirror
+local pie_dir_mirror_enabled = true
+local pie_dir_y = 1300
+
 local PATHS = {
     overlay_png = "/home/alt/mcsr/wayfiles/measuring_overlay.png",
     ninbot_jar  = "/home/alt/mcsr/wayfiles/Ninjabrain-Bot-1.5.1.jar",
 }
 
-local sens = 1.0
-
-local primary_col = "#d08e2b"
-local secondary_col = "#222222"
-local is_chat_mode = false
-local chat_mode_text = nil
-
-local config = {
-    input = {
-        layout = "norge2",
-        repeat_rate = 20,
-        repeat_delay = 150,
-    },
-    theme = {
-        ninb_anchor = "topright",
-        background = "#000000"
-    }
+local remaps_ingame = {
+    ["MB4"] = "F3",
+    ["MB5"] = "L", -- for chat key for search crafting
+    ["V"] = "BACKSPACE",
+    ["LEFTCTRL"] = "RIGHTSHIFT",
+    ["CAPSLOCK"] = "LEFTCTRL",
+    ["GRAVE"] = "ESC",
+    ["H"] = "0"
 }
+
+local remaps_chat_mode = {
+    ["MB4"] = "F3",
+    ["MB5"] = "L", -- for chat key for search crafting
+    ["GRAVE"] = "ESC",
+    ["CAPSLOCK"] = "LEFTCTRL",
+}
+
+-- CONFIGS OVER - don't edit these (unless you want to make a custom config)
+local is_chat_mode = false
+local chat_mode_text_inst = nil
 
 local function toggler(make_fn)
     local this
@@ -63,13 +104,20 @@ local function toggle_ninb()
     helpers.toggle_floating()
 end
 
-local overlay_width = 1600
-local overlay_height = 900
 
-local eye_overlay = image(PATHS.overlay_png, { x = 0, y = 400, w = overlay_width, h = overlay_height })
+local pie_dir_rows = 8
+local pie_dir_size_mult = 4
+local pie_dir_width = 100
+local pie_dir_mirror_height = 8 * pie_dir_rows * pie_dir_size_mult
+local pie_dir_x = 3840 - pie_dir_width * pie_dir_size_mult
 
-local pie_output_size = 500
-local dst_pie = { x = 2500, y = 1100, w = pie_output_size, h = pie_output_size }
+local pie_dir_mirror = mirror({ x = 3490, y = 1882, w = pie_dir_width, h = 8 * pie_dir_rows },
+    { x = pie_dir_x, y = pie_dir_y, w = pie_dir_width * pie_dir_size_mult, h = pie_dir_mirror_height })
+local is_pie_dir_mirror_visible = false
+
+local eye_overlay = image(PATHS.overlay_png, { x = overlay_x, y = overlay_y, w = overlay_width, h = overlay_height })
+local dst_pie = { x = pie_x, y = pie_y, w = pie_output_size, h = pie_output_size }
+
 local function make_pie(src)
     return {
         ent  = mirror(src, dst_pie, "#e446c4", secondary_col),
@@ -84,6 +132,7 @@ local function make_percent_mirrors(start_x, start_y)
     local width = 32
     local height = 8
     local dest_mult = 8
+    local ent_color = "#e96d4d"
 
     local dest = { x = dst_pie.x, y = dst_pie.y + pie_output_size + 50, w = width * dest_mult, h = height * dest_mult }
 
@@ -91,37 +140,49 @@ local function make_percent_mirrors(start_x, start_y)
         top = mirror(
             { x = start_x, y = start_y, w = width, h = height },
             dest,
-            "#e96d4d",
+            ent_color,
             primary_col
         ),
         middle = mirror(
             { x = start_x, y = start_y + height, w = width, h = height },
             dest,
-            "#e96d4d",
+            ent_color,
             primary_col
         ),
         bottom = mirror(
             { x = start_x, y = start_y + height * 2, w = width, h = height },
             dest,
-            "#e96d4d",
+            ent_color,
             primary_col
         ),
     }
 end
 
+local ecount_mirror_width = 76
+local ecount_mirror_height = 14
 local mirrors = {
     eye_measure  = mirror(
         { x = 162, y = 7902, w = 60, h = 580 },
         {
-            x = 0,
-            y = 400,
+            x = overlay_x,
+            y = overlay_y,
             w = overlay_width,
             h = overlay_height
         }
     ),
     f3           = {
-        ecount = mirror({ x = 0, y = 76, w = 76, h = 14 }, { x = 1000, y = 1000, w = 76 * 6, h = 14 * 6 }, "#dddddd",
-            "#ffffff"),
+        ecount = mirror(
+            { x = 0, y = 76, w = ecount_mirror_width, h = ecount_mirror_height },
+            {
+                x = ecount_mirror_x,
+                y = ecount_mirror_y,
+                w = ecount_mirror_width * ecount_mirror_size_mult,
+                h = ecount_mirror_height *
+                    ecount_mirror_size_mult
+            },
+            "#dddddd",
+            ecount_text_color
+        ),
     },
     thin_pie     = make_pie({ x = 470, y = 1759, w = 320, h = 170 }),
     thin_percent = make_percent_mirrors(708, 1940),
@@ -185,23 +246,57 @@ local function chatmode()
 
     if is_chat_mode then
         waywall.set_keymap({
-            layout = "norge2"
+            layout = ingame_layout
         })
 
-        if chat_mode_text then
-            chat_mode_text:close()
-            chat_mode_text = nil
+        waywall.set_remaps(remaps_ingame)
+
+        if chat_mode_text_inst then
+            chat_mode_text_inst:close()
+            chat_mode_text_inst = nil
         end
     else
         waywall.set_keymap({
-            layout = "us"
+            layout = chatting_layout
         })
 
-        chat_mode_text = waywall.text("CHAT MODE ENABLED", { x = 60, y = 1380, color = "#FFFFFF", size = 6 })
+        waywall.set_remaps(remaps_chat_mode)
+
+        chat_mode_text_inst = waywall.text(
+            chat_mode_text,
+            { x = chat_mode_text_x, y = chat_mode_text_y, color = chat_mode_text_color, size = chat_mode_text_size }
+        )
     end
 
     is_chat_mode = not is_chat_mode
 end
+
+local function pie_dir()
+    if is_pie_dir_mirror_visible then
+        pie_dir_mirror(false)
+        is_pie_dir_mirror_visible = false
+    else
+        if not pie_dir_mirror_enabled then
+            return
+        end
+
+        pie_dir_mirror(true)
+        is_pie_dir_mirror_visible = true
+    end
+end
+
+-- More configs, edit these to your liking
+local config = {
+    input = {
+        layout = ingame_layout,
+        repeat_rate = 40,
+        repeat_delay = 350,
+    },
+    theme = {
+        ninb_anchor = "topright",
+        background = "#000000"
+    }
+}
 
 config.actions = {
     ["*-Y"] = resize(3840, 800),
@@ -219,12 +314,10 @@ config.actions = {
     end,
     ["*-F7"] = exec_ninb,
     ["*-F8"] = toggle_ninb,
+    ["*-F2"] = pie_dir,
     ["*-F3"] = chatmode,
 }
 
-config.input.remaps = {
-    ["MB4"] = "F3",
-    ["LEFTCTRL"] = "RIGHTSHIFT",
-}
+config.input.remaps = remaps_ingame
 
 return config
